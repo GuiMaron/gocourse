200e9505192ca6720a31b9f63c94c24cb38101b5
Guilherme Maron <guimaron@gmail.com>
2021-10-14 16:00:44 +0200

    Screen.size_inches changed from float to int

--- pcbook/sample/generator.go
+++ pcbook/sample/generator.go
@@ -1,14 +1,111 @@
 package sample
 
-import "github.com/GuiMaron/gocourse/pcbook"
+import (
+	pcbook "github.com/GuiMaron/gocourse/pcbook/pb"
+)
 
-func NewKeyboard() *pb.Keyboard {
+func NewCPU () *pcbook.CPU {
 
-	keyboard := &pb.Keyboard{
+	brand 		:= randomCPUBrand()
+	name  		:= randomCPUName(brand)
+	cores		:= randomInt(2, 8)
+	threads		:= randomInt(cores, (cores * 2))
+	minClock	:= randomFloat32(2.0, 3.5)
+	maxClock	:= randomFloat32(minClock, 5.0)
+
+	cpu := &pcbook.CPU {
+		Brand:		brand,
+		Name:		name,
+		Cores:		uint32(cores),
+		Threads:	uint32(threads),
+		MinClock: 	minClock,
+		MaxClock: 	maxClock,
+	}
+
+	return cpu
+
+}
+
+func NewGPU () *pcbook.GPU {
+
+	brand 		:= randomCPUBrand()
+	name  		:= randomGPUName(brand)
+	minClock	:= randomFloat32(1.0, 1.5)
+	maxClock	:= randomFloat32(minClock, 2.0)
+
+	memory		:= &pcbook.Memory {
+		Value:	uint64(randomInt(2, 6)),
+		Unit: 	pcbook.Memory_GIGABYTE,
+	}
+
+	gpu := &pcbook.GPU {
+		Brand:		brand,
+		Name:		name,
+		MinClock: 	minClock,
+		MaxClock: 	maxClock,
+		Memory: 	memory,
+	}
+
+	return gpu
+
+}
+
+func NewHDD () *pcbook.Storage {
+
+	hdd	:= &pcbook.Storage {
+		Driver: pcbook.Storage_HDD,
+		Memory: &pcbook.Memory {
+			Value: 	uint64(randomInt(1, 6)),
+			Unit:	pcbook.Memory_TERABYTE,
+		},
+	}
+
+	return hdd
+
+}
+
+func NewKeyboard () *pcbook.Keyboard {
+
+	keyboard := &pcbook.Keyboard{
 		Layout:  randomKeyboardLayout(),
 		Backlit: randomBool(),
 	}
 
 	return keyboard
+
+}
+
+func NewRam () *pcbook.Memory {
+
+	ram	:= &pcbook.Memory {
+		Value: 	uint64(randomInt(4, 64)),
+		Unit:	pcbook.Memory_GIGABYTE,
+	}
+
+	return ram
+
+}
+
+func NewScreen () *pcbook.Screen {
+
+	screen	:= &pcbook.Screen {
+		SizeInches:	uint32(randomInt(13, 17)),
+	}
+
+	return screen
+
+}
+
+func NewSSD () *pcbook.Storage {
+
+	ssd	:= &pcbook.Storage {
+		Driver: pcbook.Storage_SSD,
+		Memory: &pcbook.Memory {
+			Value: 	uint64(randomInt(128, 1024)),
+			Unit:	pcbook.Memory_GIGABYTE,
+		},
+	}
+
+	return ssd
 
 }
--- pcbook/sample/random.go
+++ pcbook/sample/random.go
@@ -1,7 +1,8 @@
 package sample
 
 import (
-	"github.com/GuiMaron/gocourse/pcbook"
+	pcbook "github.com/GuiMaron/gocourse/pcbook/pb"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -10,23 +11,125 @@ import (
  *	General
  */
 
-func getRand() *rand.Rand {
+func getRand () *rand.Rand {
 	return rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
-func randomBool() bool {
+func randomBool () bool {
 	return (getRand().Intn(2) % 2) == 0
 }
 
-/**
- * Keyboard
- */
-func randomKeyboardLayout() *pb.Keyboard {
+func randomFloat32 (min float32, max float32) float32 {
 
-	switch getRand().Intn(5) {
+	min = float32(math.Min(float64(min),  float64(max)))
+	max = float32(math.Max(float64(min),  float64(max)))
+
+	return min + (getRand().Float32() * (max - min))
+
+}
+
+func randomInt (min int, max int) int {
+
+	min = int(math.Min(float64(min),  float64(max)))
+	max = int(math.Max(float64(min),  float64(max)))
+
+	return min + getRand().Intn(max - min + 1)
+
+}
+
+func randomStringFromSet (strings ... string) string {
+
+	length := len(strings)
+
+	if 0 == length {
+		return ""
+	}
+
+	return strings[getRand().Intn(length)]
+
+}
+
+/**
+ *	CPU
+ */
+func randomCPUBrand () string {
+	return randomStringFromSet("INTEL", "AMD")
+}
+
+func randomCPUName (brand string) string {
+
+	switch brand {
+
+	case "Intel":
+
+		return randomStringFromSet(
+			"Xeon E-2286M",
+					"Core i9-9980HK",
+					"Core i7-9750H",
+					"Core i5-9400F",
+					"Core i3-1005G1",
+				)
+
+	default:
+
+		return randomStringFromSet(
+			"Ryzen 7 PRO 2700U",
+					"Ryzen 5 PRO 3500U",
+					"Ryzen 3 PRO 3200GE",
+				)
+
+	}
+
+}
+
+/**
+ *	GPU
+ */
+func randomGPUBrand () string {
+	return randomStringFromSet("NVIDIA", "AMD")
+}
+
+func randomGPUName (brand string) string {
+
+	switch brand {
+
+	case "NVIDIA":
+
+		return randomStringFromSet(
+			"RTX 2060",
+					"RTX 2070",
+					"GTX 1660-Ti",
+					"GTX 1070",
+				)
+
+	default:
+
+		return randomStringFromSet(
+			"RX 590",
+					"RX 580",
+					"RX 5700-XT",
+					"RX Vega-56",
+				)
+
+	}
+
+}
+
+/**
+ * 	Keyboard
+ */
+func randomKeyboardLayout () pcbook.Keyboard_Layout {
+
+	switch getRand().Intn(4) {
 
 	case 1:
-		return pb.Keyboard_QWERTY
+		return pcbook.Keyboard_QWERTY
+	case 2:
+		return pcbook.Keyboard_QWERTZ
+	case 3:
+		return pcbook.Keyboard_AZERTY
+	default:
+		return pcbook.Keyboard_DVORAK
 
 	}
 
